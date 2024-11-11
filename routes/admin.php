7c7997ccde5c184f2b5c0c0fbf53a34b2a4505b1
DuyMax2005 <duytuyetdinh@gmail.com>
2024-11-11 23:39:39 +0700

    fea: complete CRUD-theater

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,13 @@ $action = $_GET['action'] ?? '/';
 
 match ($action) {
     '/' => (new DashboardController)->index(),
+
+    // CRUD Theater
+    'theaters-index'    => (new TheaterController)->index(),    // Hiển thị danh sách
+    'theaters-show'     => (new TheaterController)->show(),     // Hiển thị danh sách
+    'theaters-create'   => (new TheaterController)->create(),   // Hiển thị danh sách
+    'theaters-store'    => (new TheaterController)->store(),    // Hiển thị danh sách
+    'theaters-edit'     => (new TheaterController)->edit(),     // Hiển thị danh sách
+    'theaters-update'   => (new TheaterController)->update(),   // Hiển thị danh sách
+    'theaters-delete'   => (new TheaterController)->delete(),   // Xoá dữ liệu theo ID
 };
--- views/admin/main.php
+++ views/admin/main.php
@@ -1,5 +1,18 @@
-<?php 
+<?php include "./assets/inc/admin/header.php" ?>
 
-echo "Trang chu Admin";
+<!-- Code bên trong admin-ct-right -->
+<div class="admin-ct-right">
+    <div class="container">
+        <h1 class="mt-3 mb-3"><?= $title ?? 'Admin Dashboard' ?></h1>
 
-?>
+        <div class="row">
+            <?php
+                if(isset($view)){
+                    require_once PATH_VIEW_ADMIN . $view . '.php';
+                }
+            ?>
+        </div>
+    </div>
+</div>
+
+<?php include "./assets/inc/admin/sidebar.php" ?>
