2663f3cfced915dfaa91fad0d74d7ba9ddd1ee51
DuyMax2005 <duytuyetdinh@gmail.com>
2024-11-21 16:05:02 +0700

    hoan thien khung tren chi tiet phim

--- views/client/movie/show-movie.php
+++ views/client/movie/show-movie.php
@@ -1,4 +1,4 @@
-<div class="bg-div text-white">
+<div class="bg-detail text-white mt-2 mb-3">
     <div class="container py-3">
         <div class="row row-sm">
             <div class="d-none d-sm-block col-2">
@@ -6,21 +6,21 @@
                     <img src="<?= BASE_ASSETS_UPLOADS . 'movies/1731672540-ai_oan_trong_vuon_xuan.jpg' ?>" class="img-fluid rounded border ls-is-cached lazyloaded">
                 </a>
             </div>
-            <div class="col-12 colo-sm-10">
-                <div class="mb-3 text-center text-sm-left">
+            <div class="col-12 col-sm-10">
+                <div class="mb-3 text-sm-left">
                     <h1 class="mb-0 text-truncate">Tên phim</h1>
-                    <p class="mb-0 text-muted text-truncate">Thể loại</p>
+                    <p class="mb-0 text-truncate">Thể loại</p>
                 </div>
                 <div class="row">
                 <div class="col-12 col-lg-7">
-                    <div class="btn-block text-sm-left text-center mb-3">
+                    <div class="btn-block text-sm-left mb-3">
                         <a href="" class="btn btn-danger btn-sm">Mua vé</a>
                     </div>
                     <p class="mb-3 text-jusstify">Linh Miêu: Quỷ Nhập Tràng lấy cảm hứng từ truyền thuyết dân gian về “quỷ nhập tràng” để xây dựng cốt truyện. Phim lồng ghép những nét văn hóa đặc trưng của Huế như nghệ thuật khảm sành - một văn hóa đặc sắc của thời nhà Nguyễn, đề cập đến các vấn đề về giai cấp và quan điểm trọng nam khinh nữ. Đặc biệt, hình ảnh rước kiệu thây ma và những hình nhân giấy không chỉ biểu trưng cho tai ương hay điềm dữ mà còn là hiện thân của nghiệp quả.</p>
                     <div class="row mb-3">
                     <div class="col text-center text-sm-left">
                         <strong>
-                            <i class="fe fe-calendar">
+                            <i class="">
                                 
                             </i>
                             <span class="d-none d-sm-inline-block">Khởi chiếu</span>
@@ -30,7 +30,7 @@
                     </div>
                     <div class="col text-center text-sm-left">
                     <strong>
-                            <i class="fe fe-calendar">
+                            <i class="">
                                 
                             </i>
                             <span class="d-none d-sm-inline-block">Thời lượng</span>
@@ -40,7 +40,7 @@
                     </div>
                     <div class="col text-center text-sm-left">
                     <strong>
-                            <i class="fe fe-calendar">
+                            <i class="">
                                 
                             </i>
                             <span class="d-none d-sm-inline-block">Giới hạn tuổi</span>
