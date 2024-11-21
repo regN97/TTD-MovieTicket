ec263547524ee8e5491f2c039d434654643e4b7e
regNiqa <regniquanh@gmail.com>
2024-11-21 21:20:58 +0700

    fea: Hien thi phim chi tiet va cac truong du lieu lien quan

--- views/client/movie/show-movie.php
+++ views/client/movie/show-movie.php
@@ -8,15 +8,26 @@
             </div>
             <div class="col-12 col-sm-10">
                 <div class="mb-3 text-sm-left">
-                    <h1 class="mb-0 text-truncate"><?= $movies['name'] ?></h1>
-                    <p class="mb-0 text-truncate">Thể loại</p>
+                    <h1 class="mb-0 text-truncate"><?= mb_convert_case($movies['name'], MB_CASE_TITLE, "UTF-8"); ?></h1>
+                    <p class='mb-0 text-truncate'>
+                        <?php
+                        $arr = [];
+                        foreach ($movieGenre as $mg) {
+                            $arr[] = $mg['g_name'];
+                        }
+
+                        $actor = implode(", ", $arr);
+
+                        echo $actor;
+                        ?>
+                    </p>
                 </div>
                 <div class="row">
                     <div class="col-12 col-lg-7">
                         <div class="btn-block text-sm-left mb-3">
                             <a href="" class="btn btn-danger btn-sm">Mua vé</a>
                         </div>
-                        <p class="mb-3 text-jusstify">Linh Miêu: Quỷ Nhập Tràng lấy cảm hứng từ truyền thuyết dân gian về “quỷ nhập tràng” để xây dựng cốt truyện. Phim lồng ghép những nét văn hóa đặc trưng của Huế như nghệ thuật khảm sành - một văn hóa đặc sắc của thời nhà Nguyễn, đề cập đến các vấn đề về giai cấp và quan điểm trọng nam khinh nữ. Đặc biệt, hình ảnh rước kiệu thây ma và những hình nhân giấy không chỉ biểu trưng cho tai ương hay điềm dữ mà còn là hiện thân của nghiệp quả.</p>
+                        <p class="mb-3 text-jusstify"><?= $movies['description'] ?></p>
                         <div class="row mb-3">
                             <div class="col text-center text-sm-left">
                                 <strong>
@@ -26,7 +37,7 @@
                                     <span class="d-none d-sm-inline-block">Khởi chiếu</span>
                                 </strong>
                                 <br>
-                                <span>22/11/2024</span>
+                                <span><?= date_format(date_create($movies['release_date']), "d/m/Y") ?></span>
                             </div>
                             <div class="col text-center text-sm-left">
                                 <strong>
@@ -36,7 +47,7 @@
                                     <span class="d-none d-sm-inline-block">Thời lượng</span>
                                 </strong>
                                 <br>
-                                <span>109 phút</span>
+                                <span><?= $movies['duration'] ?> phút</span>
                             </div>
                             <div class="col text-center text-sm-left">
                                 <strong>
@@ -46,7 +57,7 @@
                                     <span class="d-none d-sm-inline-block">Giới hạn tuổi</span>
                                 </strong>
                                 <br>
-                                <span>T18</span>
+                                <span><?= $movies['type'] ?></span>
                             </div>
                         </div>
                     </div>
@@ -55,16 +66,36 @@
                             <strong>Diễn viên</strong>
                             <br>
                             <span>
-                                <a href="" class="text-danger">Nguyễn Văn A</a>,
-                                <a href="" class="text-danger">Trần Thị B</a>
+                                <?php
+                                $arr = [];
+                                foreach ($movieArtist as $ma) {
+                                    if ($ma['a_roles'] == 'Diễn viên') {
+                                        $arr[] = "<a href='' class='text-danger'>{$ma['a_name']}</a>";
+                                    }
+                                }
+
+                                $actor = implode(", ", $arr);
+
+                                echo $actor;
+                                ?>
                             </span>
                         </p>
                         <p class="mb-2">
                             <strong>Đạo diễn</strong>
                             <br>
                             <span>
-                                <a href="" class="text-danger">Nguyễn Văn A</a>,
-                                <a href="" class="text-danger">Trần Thị B</a>
+                                <?php
+                                $arr = [];
+                                foreach ($movieArtist as $ma) {
+                                    if ($ma['a_roles'] == 'Đạo diễn') {
+                                        $arr[] = "<a href='' class='text-danger'>{$ma['a_name']}</a>";
+                                    }
+                                }
+
+                                $actor = implode(", ", $arr);
+
+                                echo $actor;
+                                ?>
                             </span>
                         </p>
                     </div>
